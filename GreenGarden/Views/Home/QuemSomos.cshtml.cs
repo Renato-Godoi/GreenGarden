8ab8225c559458073d5dab6ae90ba99099962ba9
Renato <Renato@DESKTOP-DM7TG34>
2024-11-18 21:17:03 -0300

    Adicionar arquivos de projeto.

---
+++ GreenGarden/Views/Home/QuemSomos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GreenGarden.Views.Home
+{
+    public class QuemSomosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
